c821659e25c7dfd2bb2b7c1b24dd2c7cc2ca4267
JeromeBersin <bersin.jerome@gmail.com>
2021-04-12 15:46:43 +0200

    création footer sans styles

---
+++ src/components/App/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default function() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="navbar__list"><a href="#">Contact</a></li>
+        <li className="navbar__list"><a href="#"></a>About Us</li>
+      </ul>
+    </nav>
+  );
+}
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,14 @@
 import React from 'react';
 
 // == Import
+import Footer from './Footer';
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
     <h1>Composant : App</h1>
+    <Footer />
   </div>
 );
 
