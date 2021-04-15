5a1692033bb02ec00614ff8bf4d7facf464450f7
Cide74 <regis.blec@gmail.com>
2021-04-15 13:33:10 +0200

    clean code

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-// import Img404 from 'public/img/404.jpg'
-// import Img404 from './404.jpg'
+
 import "./styles.scss";
 
 const Error = () => {
   return (
     <div className="error"
         title="Erreur">
-         
-                <h1 className="error__title">
+        <h1 className="error__title">
         "Ah ! Une erreur s'est produite."
-        </h1>  {/* 
-        <img className="error__image"
-        src={Img404} alt="404" />*/} 
-
+        </h1>
     </div>
   );
 }
