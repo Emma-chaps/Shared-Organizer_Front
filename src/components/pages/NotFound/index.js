f0bba90205c574a1ba4cabcfd15aaca5f7f5b4cb
Cide74 <regis.blec@gmail.com>
2021-04-15 11:30:15 +0200

    styles BEM Home

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 // import Img404 from 'public/img/404.jpg'
 import Img404 from './404.jpg'
+import "./styles.scss";
 
-const Error = () => (
-  <div
-    
-      title="Erreur">
-      <h1>"Nous sommes désolé, Une erreur s'est produite."</h1>
-      <img src={Img404} alt="404" />
-   
-</div>
-);
+const Error = () => {
+  return (
+    <div className="error"
+        title="Erreur">
+          {/* 
+        <img className="error__image"
+        src={Img404} alt="404" />*/}
+        <h1 className="error__title">
+        "A Une erreur s'est produite."
+        </h1>
+    </div>
+  );
+}
 
 export default Error;
